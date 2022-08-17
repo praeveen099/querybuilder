88466cf352a0f7022c3f038df11a7db3d538bbbe
Praeveen Ganesharaja <praeveen099@gmail.com>
2022-08-17 15:12:53 +0100

    Allowed posts to be created

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,18 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostsController extends Controller
 {
 
     public function index()
     {
-        return view('posts');
+
+        $posts = DB::table('posts')->paginate(15);
+
+        $numberOfPosts = DB::table('posts')->count();
+
+        return view('posts', ['posts' => $posts, 'numberOfPosts' => $numberOfPosts]);
     }
 
     public function create()
     {
-        return view('posts');
+        $attributes = request()->validate([
+            'title' => 'required',
+            'excerpt' => 'required',
+            'body' => 'required',
+            'user_id' => 'required',
+        ]);
+
+        DB::table('posts')->insert([
+            'title' => $attributes['title'],
+            'excerpt' => $attributes['excerpt'],
+            'body' => $attributes['body'],
+            'user_id' => $attributes['user_id'],
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        return redirect('/posts');
 
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,18 +2,35 @@
 
 @section('content')
 
-    <h1>Posts</h1>
 
+    <table>
+        <tr>
+            <th>Title</th>
+            <th>Excerpt</th>
+            <th>Author ID</th>
+          </tr>
+        @foreach ($posts as $post)
+        <tr>
+            <td>{{ $post->title }}</td>
+            <td>{{ $post->excerpt }}</td>
+            <td>{{ $post->user_id }}</td>
+        </tr>
+
+
+        @endforeach
+
+    </table>
+    {{ $posts->links() }}
     <form method="POST" action="/posts">
         @csrf
-        <label for="name">Title:</label>
-        <input type="text" id="title" name="title"><br>
-        <label for="name">Excerpt:</label>
-        <input type="text" id="excerpt" name="excerpt"><br>
-        <label for="name">Body:</label>
-        <input type="text" id="body" name="body"><br>
-        <label for="name">Author id:</label>
-        <input type="number" id="user_id" name="user_id" min="1" max="100"><br>
+        <label for="title">Title:</label>
+        <input type="text" id="title" name="title" required><br>
+        <label for="excerpt">Excerpt:</label>
+        <input type="text" id="excerpt" name="excerpt" required><br>
+        <label for="body">Body:</label>
+        <input type="text" id="body" name="body" required><br>
+        <label for="user_id">Author id:</label>
+        <input type="number" id="user_id" name="user_id" min="1" max="{{ $numberOfPosts }}" required><br>
         <input type="submit" value="Submit">
 
     </form>
